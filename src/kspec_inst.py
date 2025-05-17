d0729bffeacc1da27779d08a20c3458fe8d7e393
hbahk <bahkhyeonguk@gmail.com>
2025-05-17 23:22:44 +0900

    Add KspecTdfio class for Kspec-specific functionality in Tdfio
    This commit introduces the KspecTdfio class, a subclass of Tdfio, which adds functionality specific to the Kspec instrument. It includes an instrument property that retrieves the instrument type based on the "INSTRUME" FITS keyword, with error handling for missing or empty keywords.

--- src/kspec_inst.py
+++ src/kspec_inst.py
@@ -1,4 +1,4 @@
-from twodfdr.io import TdfioInstType
+from twodfdr.io import Tdfio, TdfioInstType, TdfioKeyType
 
 class KspecTdfioInstType(TdfioInstType):
     """
@@ -44,6 +44,44 @@ class KspecTdfioInstType(TdfioInstType):
     """Hector-AAOmega"""
     SPECTOR_HECTOR= 13
     """Spector-AAOmega"""
-    
+
+class KspecTdfio(Tdfio):
+    """
+    A subclass of Tdfio that adds Kspec-specific functionality.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+           
+    @property
+    def instrument(self):
+        """
+        Returns the code for the instrument that produced the current file.
+
+        Notes
+        -----
+        A central database for instrument names. Used to tranlate the value
+        given in the file's "INSTRUME" FITS keyword to the corresponding code
+        (see TDFRED_PAR). The code is then normally used is a SELECT statement.
+
+        Returns
+        -------
+        TdfioInstType
+            The instrument type of the current file.
+        """
+        if not self.key_exists("INSTRUME"):
+            raise RuntimeError("Missing INSTRUME keyword. Cannot determine instrument type.")
+
+        (inst_str,inst_comment) = self.key_read("INSTRUME",TdfioKeyType.STR)
+        inst_str = inst_str.upper()
+
+        if len(inst_str) == 0:
+            raise RuntimeError("INSTRUME keyword is empty. Cannot determine instrument type.")
+
+        if inst_str == 'KSPEC':
+            # KSPEC is not in TdfioInstType
+            # remove this once KSPEC is fully integrated
+            return KspecTdfioInstType.KSPEC
+        else:
+            return super().instrument
 
     
